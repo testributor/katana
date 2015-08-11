115fc5b64f3ce4ab6d16d21b55ee41f47e3ae926
Dimitris Karakasilis <jimmykarily@gmail.com>
2015-08-11 21:43:56 +0300

    Use initializer set config in config/database.yml

---
+++ config/initializers/database_credentials.sample.rb
@@ -0,0 +1,8 @@
+LOCAL_DB = {
+  dev_database: "katana",
+  dev_database_username: "katana",
+  dev_database_password: nil
+  test_database: "katana_test"
+  test_database_username: "katana",
+  test_database_password: nil
+}
