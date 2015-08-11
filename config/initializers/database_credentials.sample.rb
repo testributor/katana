d70e31258a3afcbf4393984939468df01110c8a7
Dimitris Karakasilis <jimmykarily@gmail.com>
2015-08-11 21:59:25 +0300

    Add missing commas from sample configuration file and fix database.yml

--- config/initializers/database_credentials.sample.rb
+++ config/initializers/database_credentials.sample.rb
@@ -1,8 +1,8 @@
 LOCAL_DB = {
   dev_database: "katana",
   dev_database_username: "katana",
-  dev_database_password: nil
-  test_database: "katana_test"
+  dev_database_password: nil,
+  test_database: "katana_test",
   test_database_username: "katana",
   test_database_password: nil
 }
