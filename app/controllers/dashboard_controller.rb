906e36cda06050493a5664bf54c6436e946374c3
spyrbri <spyrbri@gmail.com>
2015-08-13 02:03:55 +0300

    add some styling

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,4 @@
 class DashboardController < ApplicationController
-  layout "dashboard"
-
   before_filter :authenticate_user!
 
   def index
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def brand_name
+    'Katanomeus'
+  end
 end
