d1536f961324d4446c66168056aa6ff5ae3c3f6e
Pavlos Kallis <pavlos@incrediblue.com>
2015-09-02 19:50:18 +0300

    Add basic js infrastructure
    To enable running code per specific page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require underscore
 //= require turbolinks
 //= require bootstrap-sprockets
+//= require init
 //= require_tree .
---
+++ app/assets/javascripts/pages/test_jobs.coffee
@@ -0,0 +1,4 @@
+Testributor.Pages ||= {}
+class Testributor.Pages.TestJobs
+  index: ->
+    console.log("hello test jobs")
