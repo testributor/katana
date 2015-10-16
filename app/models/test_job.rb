a06e071ab80ebae94cc9ebbc9b8e40dceef89ad9
Pavlos Kallis <pavlos@incrediblue.com>
2015-10-16 15:02:35 +0300

    Write tests for TestJob#total_running_time

--- app/models/test_job.rb
+++ app/models/test_job.rb
@@ -4,6 +4,7 @@ class TestJob < ActiveRecord::Base
   belongs_to :user
   belongs_to :tracked_branch
   has_one :project, through: :tracked_branch
+
   delegate :completed_at, to: :last_file_run
 
   scope :pending, -> { where(status: TestStatus::PENDING) }
@@ -25,11 +26,14 @@ class TestJob < ActiveRecord::Base
       pluck(:completed_at)
     started_at_times = test_job_files.order("started_at ASC").
       pluck(:started_at)
-    if completed_at_times.length == completed_at_times.compact
-      completed_at_times.last - started_at_times.first
+
+    if completed_at_times.length == completed_at_times.compact.length
+      time = completed_at_times.last - started_at_times.first
     elsif time_first_job_started = started_at_times.compact.first
-      Time.now - time_first_job_started
+      time = Time.now - time_first_job_started
     end
+
+    time.round
   end
 
   def build_test_job_files
