79e1c60547cf2e8e24a8ce2979ae520f80a9d9e1
Ilias Spyropoulos <ilias@heavymelon.com>
2015-11-08 13:09:57 +0200

    Fix incorrect migration file
    Switched from #change to #up/#down methods to allow database to
    roll back from the change.

--- db/migrate/20151019145044_change_projects_limit_default_to_users.rb
+++ db/migrate/20151019145044_change_projects_limit_default_to_users.rb
@@ -1,5 +1,9 @@
 class ChangeProjectsLimitDefaultToUsers < ActiveRecord::Migration
-  def change
+  def up
     change_column_default :users, :projects_limit, 1
+  end
+
+  def down
+    change_column_default :users, :projects_limit, 0
   end
 end
