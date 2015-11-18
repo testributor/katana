f4a09865b06d0b3153006836dc19c4695e8503fd
Dimitris Karakasilis <jimmykarily@gmail.com>
2015-11-18 18:26:38 +0200

    Fix breaking test

--- test/factories/project.rb
+++ test/factories/project.rb
@@ -3,5 +3,10 @@ FactoryGirl.define do
     sequence(:name) { |n| "ACME #{n}" }
     association :user
     sequence(:repository_id) { |n| n }
+
+    after(:create) do |project, evaluator|
+      project.docker_image = FactoryGirl.create(:docker_image)
+      project.save!
+    end
   end
 end
