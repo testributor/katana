6bde71703284cc520fa1039e284d41db15dd4816
Pavlos Kallis <pakallis@gmail.com>
2015-11-18 11:32:29 +0200

    Add repos cassette and fix broken test

--- test/features/project_wizard_feature_test.rb
+++ test/features/project_wizard_feature_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 class ProjectWizardFeatureTest < Capybara::Rails::TestCase
   let(:user) { FactoryGirl.create(:user) }
+  let(:repo_name) { 'ispyropoulos/katana' }
 
   before do
     login_as user, scope: :user
@@ -9,13 +10,14 @@ class ProjectWizardFeatureTest < Capybara::Rails::TestCase
 
   it "user can't click on disabled(not-completed) steps", js: true do
     visit root_path
-    VCR.use_cassette('repos') do
-      find('aside').click_on 'Add a project'
-      page.must_have_content 'ispyropoulos/katana'
+    VCR.use_cassette 'repos'  do
+      VCR.use_cassette 'github_client' do
+        find('aside').click_on 'Add a project'
+        page.must_have_content repo_name
+      end
     end
-    VCR.use_cassette('branches') do
-      click_on "Select branches"
-    end
+
+    click_on "Select branches"
 
     page.wont_have_content "You need to select a repository first"
     page.must_have_content "Select a repository"
