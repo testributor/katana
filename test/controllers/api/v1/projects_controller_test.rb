6926a9b5afda8c57af65168f2941de2807811a07
Dimitris Karakasilis <jimmykarily@gmail.com>
2015-11-20 19:33:55 +0200

    Add missing test file

---
+++ test/controllers/api/v1/projects_controller_test.rb
@@ -0,0 +1,55 @@
+require 'test_helper'
+
+class Api::V1::ProjectsControllerTest < ActionController::TestCase
+  let(:project) { FactoryGirl.create(:project) }
+  let(:application) do
+    FactoryGirl.create(:doorkeeper_application, owner: project)
+  end
+  let(:token) { application.access_tokens.create }
+  let(:project_files) do
+    FactoryGirl.create_list(:project_file, 2, project: project)
+  end
+
+  before { project_files }
+
+  describe "GET#current" do
+    it "returns the current project with project_files included" do
+      get :current, access_token: token.token,
+        default: { format: :json }
+
+      result = JSON.parse(response.body)
+      files = result["files"]
+      files.first["id"].must_equal project_files.first.id
+      files.first["path"].must_equal project_files.first.path
+      files.first["contents"].must_equal project_files.first.contents
+      files.last["id"].must_equal project_files.last.id
+      files.last["path"].must_equal project_files.last.path
+      files.last["contents"].must_equal project_files.last.contents
+
+      (%w(repository_name repository_owner github_access_token build_commands files) - result.keys).must_be :blank?
+    end
+  end
+
+  describe "active_workers (ApiController)" do
+    it "monitors the active workers" do
+      point_in_time = Time.current
+      Timecop.freeze(point_in_time) do
+        request.env['HTTP_WORKER_UUID'] = '123'
+        get :current, access_token: token.token, default: { format: :json }
+
+        project.active_workers.count.must_equal 1
+
+        request.env['HTTP_WORKER_UUID'] = '124'
+        get :current, access_token: token.token, default: { format: :json }
+
+        project.active_workers.count.must_equal 2
+      end
+
+      # Expire the workers
+      Katana::Application.redis.expire "project_#{project.id}_worker_123", 0
+      Katana::Application.redis.expire "project_#{project.id}_worker_124", 0
+
+      project.active_workers.count.must_equal 0
+    end
+  end
+end
