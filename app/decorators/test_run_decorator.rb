00cb100a9e3ad4f0149d844124a92072d0bb97b4
Ilias Spyropoulos <ilias@heavymelon.com>
2015-11-20 14:58:35 +0200

    Improve and add some visual elements
    - Fixed FORK indicator
    - Added information about commit author, committer and committed
      date.

--- app/decorators/test_run_decorator.rb
+++ app/decorators/test_run_decorator.rb
@@ -3,11 +3,12 @@ class TestRunDecorator < ApplicationDecorator
 
   def commit_message(options={})
     options.reverse_merge!(render_as_link: false)
+
     if model.commit_message.present?
       # http://stackoverflow.com/a/18134919/859387
       cm = "#{h.truncate(model.commit_message.split("\n")[0], length: 80, separator: ' ')} (##{model.commit_sha[0...7]})"
       if options[:render_as_link]
-        cm = h.link_to cm, model.commit_url, title: 'See this commit on GitHub',
+        cm = h.link_to cm, commit_url, title: 'See this commit on GitHub',
           target: '_blank'
       end
 
@@ -15,5 +16,22 @@ class TestRunDecorator < ApplicationDecorator
     else
       model.commit_sha
     end
+  end
+
+  def commit_info
+    author = if commit_author_email == commit_committer_email
+               commit_author_name
+             else
+               "#{commit_author_name} (with #{commit_committer_name})"
+             end
+    info = <<-HTML
+      #{author}
+      committed
+      <span title='#{l(commit_timestamp, format: :long)}'>
+        #{h.time_ago_in_words(commit_timestamp)} ago
+      <span>
+    HTML
+
+    info.html_safe
   end
 end
