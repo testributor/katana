bd22fa6d94cb9ada6e9b8c71204eb8c1decddb16
Pavlos Kallis <pakallis@gmail.com>
2015-11-20 13:31:45 +0200

    Fix wrong redirect url upon github authorization
    Fix wrong redirect and exception when a new user tries to authorize github

--- app/controllers/project_wizard_controller.rb
+++ app/controllers/project_wizard_controller.rb
@@ -77,7 +77,7 @@ class ProjectWizardController < WizardController
   end
 
   def fetch_repos
-    redirect_to project_wizard_add_project_path if !request.xhr?
+    redirect_to project_wizard_path(:add_project) and return if !request.xhr?
 
     @repos = @project_wizard.fetch_repos
     render 'fetch_repos', layout: false
