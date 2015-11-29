79b27846112a6570a8561a65a3d003a16fce764c
Dimitris Karakasilis <jimmykarily@gmail.com>
2015-11-29 18:05:55 +0200

    Reduce the logs produced by Rack::Timeout

--- config/initializers/timeout.rb
+++ config/initializers/timeout.rb
@@ -1,2 +1,3 @@
 # https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#timeout
 Rack::Timeout.timeout = 20  # seconds
+Rack::Timeout::Logger.level  = Logger::ERROR
