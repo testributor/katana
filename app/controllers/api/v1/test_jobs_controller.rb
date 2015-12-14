ab28cbc32bfaa79a37ee85f1979e34f941011fbd
Dimitris Karakasilis <jimmykarily@gmail.com>
2015-12-14 13:03:33 +0200

    Remove unecessary "joins" call. It is called implicitely

--- app/controllers/api/v1/test_jobs_controller.rb
+++ app/controllers/api/v1/test_jobs_controller.rb
@@ -9,7 +9,7 @@ module Api
         # Calculate workload and the number of queued or running jobs.
         # We try to equally distribute the load so we only send
         # workload / active_workers number of when jobs are requested.
-        workload = current_project.test_jobs.joins(:test_run).
+        workload = current_project.test_jobs.
           where(test_runs: { status: [TestStatus::RUNNING, TestStatus::QUEUED]}).
           where(status: [TestStatus::RUNNING,TestStatus::QUEUED]).count
 
