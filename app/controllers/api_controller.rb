9b02c9634c2bc329553e8677328ab3ae04ac2870
Dimitris Karakasilis <jimmykarily@gmail.com>
2016-02-02 12:17:29 +0200

    Create worker_uuid_redis_key method

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -9,20 +9,22 @@ class ApiController < ActionController::Base
   end
 
   def worker_uuid
-    uuid = request.headers['HTTP_WORKER_UUID']
+    request.headers['HTTP_WORKER_UUID']
+  end
 
-    "project_#{current_project.id}_worker_#{uuid}" if uuid
+  def worker_uuid_redis_key
+    "project_#{current_project.id}_worker_#{worker_uuid}" if worker_uuid
   end
 
   def worker_report
-    return false unless worker_uuid
+    return false unless worker_uuid_redis_key
 
     redis = Katana::Application.redis
     # Add an expiring key for the worker if not already there
-    redis.setnx worker_uuid, worker_uuid
-    redis.expire worker_uuid, Project::ACTIVE_WORKER_THRESHOLD_SECONDS
+    redis.setnx worker_uuid_redis_key, worker_uuid_redis_key
+    redis.expire worker_uuid_redis_key, Project::ACTIVE_WORKER_THRESHOLD_SECONDS
     # Add the worker to the project's set of workers
-    redis.sadd current_project.workers_redis_key, worker_uuid
+    redis.sadd current_project.workers_redis_key, worker_uuid_redis_key
     # update the list of active workers on reports too to keep the list
     # size small. If we cleaned the list only on "reads" and a read did not come
     # for too long, we could end up bloating the memory (that might not be an
