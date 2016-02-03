267e07801065f243e3567a8a62c8b7e4abe133c4
Dimitris Karakasilis <jimmykarily@gmail.com>
2016-02-03 17:44:17 +0200

    Really use different redis database per environment
    Redis does not allow strings as database names, only indexes so in
    reallity we were using the same database in all envs (0 that is)

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,13 +1,18 @@
 # TODO: Consider using the "with" method if it becomes a problem:
 # https://github.com/mperham/connection_pool#migrating-to-a-connection-pool
 redis_pool_size = ENV["REDIS_POOL_SIZE_PER_PROCESS"] || 5
+redis_db_index = {
+  "production" => 0,
+  "development" => 1,
+  "test" => 2,
+}
 Katana::Application.redis =
   if ENV["REDIS_URL"]
     ConnectionPool::Wrapper.new(size: redis_pool_size, timeout: 5) do
-      Redis.new(url: ENV["REDIS_URL"], db: "katana_#{Rails.env}")
+      Redis.new(url: ENV["REDIS_URL"], db: redis_db_index[Rails.env.to_s])
     end
   else
     ConnectionPool::Wrapper.new(size: redis_pool_size, timeout: 5) do
-      Redis.new(db: "katana_#{Rails.env}")
+      Redis.new(db: redis_db_index[Rails.env.to_s])
     end
   end
