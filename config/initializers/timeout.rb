a3d3899daf43751b61ab7e89296d65aa7a4d086a
Dimitris Karakasilis <jimmykarily@gmail.com>
2016-02-23 13:18:03 +0200

    Don't override the Rack::Timeout service timeout
    to avoid letting thread run with not connection available

--- config/initializers/timeout.rb
+++ config/initializers/timeout.rb
@@ -1,4 +1,13 @@
 # https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#timeout
+# Don't override the timeout unless you know what you are doing.
+# Rack::Timeout calculates the timeout value by subsctracting the wait time
+# from the default of 15 seconds of service time. When timeout is set it is
+# always honoured so setting this here might cause the dyno to continue
+# processing the request even after the heroku router has closed the connection.
+# https://github.com/heroku/rack-timeout#wait-timeout
+#
 # Disable on development (0 means disable)
-Rack::Timeout.timeout = Rails.env.development? ? 0 : 20  # seconds
+if Rails.env.development?
+  Rack::Timeout.timeout = 0 # disable
+end
 Rack::Timeout::Logger.level  = Logger::ERROR
