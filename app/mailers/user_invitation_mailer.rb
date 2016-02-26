322598a3b4d8b946202f5cd685a3c774a43d6778
Spyros Brilis <spyrbri@gmail.com>
2016-02-26 16:34:20 +0200

    insert sender information to inviation e-mail

--- app/mailers/user_invitation_mailer.rb
+++ app/mailers/user_invitation_mailer.rb
@@ -1,7 +1,10 @@
 class UserInvitationMailer < ApplicationMailer
   def new_invitation(id)
     @user_invitation = UserInvitation.find(id)
+    @project = Project.find(@user_invitation.project_id)
     @accept_url = accept_user_invitation_url(token: @user_invitation.token)
-    mail(to: @user_invitation.email, subject: 'You are invited to a new project in testributor.com')
+    mail(to: @user_invitation.email,
+         reply_to: @project.user.email,
+         subject: "You have been invited to the Testributor project: #{@project.name}.")
   end
 end
