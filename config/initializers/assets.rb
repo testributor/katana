11a92b01b09aeda0db52df692c81b36c04332433
Spyros Brilis <spyrbri@gmail.com>
2016-02-27 16:18:30 +0200

    improved about us page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,6 +8,5 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( dashboard.js dashboard.css front.css front.js codemirror.js formatting.js yaml.js shell.js custom.codemirror.js github.svg favicon-16x16.png service-logos/*)
-
+Rails.application.config.assets.precompile += %w( dashboard.js dashboard.css front.css front.js codemirror.js formatting.js yaml.js shell.js custom.codemirror.js github.svg favicon-16x16.png profile-photos/* service-logos/*)
 Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
