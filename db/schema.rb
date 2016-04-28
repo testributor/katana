fde71fa41d076cceeba849c5bdc3cda7ac97652b
Dimitris Karakasilis <jimmykarily@gmail.com>
2016-04-28 15:23:34 +0300

    Fix the order of extensions to match the production database
    This way, when we import production backup on our development dbs we
    won't see a diff. Anyone seeing a diff locally after running migrations
    should reset the local database using the new schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,8 @@ ActiveRecord::Schema.define(version: 20160424122651) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "pg_stat_statements"
   enable_extension "intarray"
+  enable_extension "pg_stat_statements"
 
   create_table "branch_notification_settings", force: :cascade do |t|
     t.integer  "project_participation_id",             null: false
