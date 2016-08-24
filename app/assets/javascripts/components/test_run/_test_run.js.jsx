5c360252435fea00477c19a10fa34ff445de635f
Pavlos Kallis <pakallis@gmail.com>
2016-08-24 21:35:15 +0300

    Fix overflow of TestRun actions and info
    In TestRuns index page, when area was small, the info and actions
    inside a test run overflowed the outer area. Now, each element takes its
    own row

--- app/assets/javascripts/components/test_run/_test_run.js.jsx
+++ app/assets/javascripts/components/test_run/_test_run.js.jsx
@@ -24,34 +24,32 @@ var TestRun = React.createClass({
 
     return (
       <div id={ 'test-run-' + this.props.testRun.id } className='panelized row'>
-        <div className="col-lg-8">
+        <div className="col-lg-9">
           <a href={ this.props.testRun.test_run_link }>
             <ProgressBars statuses={ this.props.testRun.statuses } total={ this.props.testRun.statuses.total } key={3} />
           </a>
           <TestRunCommit commit={ this.props.testRun.commit_info } key={5} />
         </div>
 
-        <div className="col-lg-2 col-xs-6">
-          <div className='row'>
-            <div className="col-lg-12">
-              <a href={ this.props.testRun.test_run_link}>
-                <div className={ this.props.testRun.status_css_class }>
-                  <span className='m-r-5'>#{ this.props.testRun.run_index }</span>
-                  <span className='m-r-5'> | </span>
-                  <span>{this.props.testRun.status_text }</span>
-                </div>
-              </a>
-            </div>
-
-            { totalRunningTime() }
-            <div className="col-lg-12 time-div">
-              <i className="fa fa-calendar" aria-hidden="true"></i>
-              <span> { this.props.testRun.created_at } </span>
-            </div>
+        <div className="col-lg-3">
+          <div>
+            <a href={ this.props.testRun.test_run_link}>
+              <div className={ this.props.testRun.status_css_class }>
+                <span className='m-r-5'>#{ this.props.testRun.run_index }</span>
+                <span className='m-r-5'> | </span>
+                <span>{this.props.testRun.status_text }</span>
+              </div>
+            </a>
           </div>
-        </div>
-        <div className="col-lg-2 col-xs-6 text-right m-t-5">
-          { testRunCtas }
+
+          { totalRunningTime() }
+          <div className="time-div m-t-10">
+            <i className="fa fa-calendar" aria-hidden="true"></i>
+            <span> { this.props.testRun.created_at } </span>
+          </div>
+          <div>
+            { testRunCtas }
+          </div>
         </div>
       </div>
     )
