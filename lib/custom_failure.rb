608213c1ee6e9a57d04b0ce0875876eff10724ae
Dimitris Karakasilis <jimmykarily@gmail.com>
2016-09-09 13:57:29 +0300

    Fix redirection when user submits a wrong password on login.

--- lib/custom_failure.rb
+++ lib/custom_failure.rb
@@ -1,7 +1,14 @@
 # https://github.com/plataformatec/devise/wiki/How-To:-Redirect-to-a-specific-page-when-the-user-can-not-be-authenticated
 class CustomFailure < Devise::FailureApp
   def redirect_url
-    ENV["UNAUTHORIZED_REDIRECT_URL"] || new_user_session_path
+    # If comming from sign_in page (as in the case of wrong password)
+    # or there is no setting to override the redirect url, use the default
+    # method.
+    if request.referer == new_user_session_url || !ENV["UNAUTHORIZED_REDIRECT_URL"]
+      super
+    else
+      ENV["UNAUTHORIZED_REDIRECT_URL"]
+    end
   end
 
   # You need to override respond to eliminate recall
